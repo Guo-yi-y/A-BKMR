ba3f942434522d039986d4dcdba6512022f511bf
Guo guo <Guo-yi-y>
2025-06-23 23:22:05 +0800

    function update

--- R/load_py_function.R
+++ R/load_py_function.R
@@ -6,26 +6,16 @@
     stop("Please install the reticulate package first.")
   }
   library(reticulate)
-  # 2. 检查 Python 是否已安装
 
 
-  if (is.null(py_discover_config())) {
-    message("Python is not installed or not detected.")
-
-    message("Attempting to install Python using reticulate::py_require")
-
-  } else {
-    message("Python is installed. Proceeding with the package load...")
-  }
-
-  # 3. 声明 Python 依赖（只执行一次，后续会话重用同一环境）
+  # 2. 声明 Python 依赖（只执行一次，后续会话重用同一环境）
   reticulate::py_require(
     packages = c("numpy", "mpmath", "pandas", "scipy")
   )
 
 
 
-  # 4. 加载 Python 文件
+  # 3. 加载 Python 文件
   python_file <- system.file("python", "py_functions.py", package = pkgname)
   if (python_file == "") {
     stop("Python file 'py_functions.py' not found.")
