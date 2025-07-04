58119939205a3e8b73251e23633431161071dec2
Guo guo <Guo-yi-y>
2025-07-04 16:44:04 +0800

    function update

---
+++ R/_PACKAGE.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+#' @import dplyr
+#' @import magrittr
+#' @import fields
+#' @import bkmr
+#' @import statmod
+#' @import glue
+#' @import reticulate
+#' @importFrom MASS mvrnorm
+"_PACKAGE"
